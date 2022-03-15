f0316b86b2dc61cb4cfcc4f7e8797fd5326178e6
Diego Guisasola <diego.guisasola@utp.edu.co>
2022-03-15 16:49:32 -0500

    Finished initial implementation of the State Pattern

---
+++ Assets/Scripts/Carrot/CarrotBaseState.cs
@@ -0,0 +1,9 @@
+﻿namespace Carrot
+{
+    public abstract class CarrotBaseState
+    {
+        public abstract void EnterState(CarrotStateManager carrot);
+        public abstract void UpdateState(CarrotStateManager carrot);
+    
+    }
+}
---
+++ Assets/Scripts/Carrot/CarrotInitialState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Carrot
+{
+    public class CarrotInitialState : CarrotBaseState
+    {
+        private float changeStateCountdown = 2.0f;
+        
+        
+        public override void EnterState(CarrotStateManager carrot)
+        {
+            Animator animator = carrot.GetComponent<Animator>();
+            animator.Play("CarrotBase");
+        }
+
+        public override void UpdateState(CarrotStateManager carrot)
+        {
+            if (changeStateCountdown >= 0)
+            {
+                changeStateCountdown -= Time.deltaTime;
+            }
+            else
+            {
+                carrot.SwitchState(carrot.RottenState);
+                changeStateCountdown = 2.0f;
+            }
+        }
+    }
+}
